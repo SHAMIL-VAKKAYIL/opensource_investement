da5b0ac1dc227ec1cc5a70827a580a17fb724f1b
shamil <muhammedshamil470@gmail.com>
2025-10-26 11:13:10 +0530

    create:complte service

---
+++ user-service/src/services/user.service.js
@@ -0,0 +1,24 @@
+import UserDetails from "../model/UserDetail";
+
+
+class UserSerivce {
+
+    async createUser(data) {
+        const { userId } = data
+        const createUser = await UserDetails.create({userId})
+        return createUser
+    }
+    async addingUserData(data) {
+        const { addUserData, userId } = data
+        const addData = await UserDetails.updateOne({ userId }, { $set: { ...addUserData } }, { new: true })
+        return addData
+    }
+    async updateUser(data) {
+        const { updateUser, userId } = data
+        const updatedUser = await UserDetails.updateOne({ userId }, { $set: { ...updateUser } }, { new: true })
+        return updatedUser
+    }
+}
+
+
+export default new UserSerivce
