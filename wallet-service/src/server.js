1a980deeb5c326d66c9cdef2769ab04dc4311ea1
shamil <muhammedshamil470@gmail.com>
2025-10-27 20:39:55 +0530

    update:start wallet service

--- wallet-service/src/server.js
+++ wallet-service/src/server.js
@@ -2,7 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db.js'
-// import authRouter from './router/auth.route.js'
+import { walletCreatedEvent } from './kafka/consumer.js'
+import walletRouter from './routes/wallet.route.js'
 
 
 dotenv.config()
@@ -11,13 +12,14 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-// app.use('/api/auth',authRouter)
+app.use('/api/wallet',walletRouter)
 
 
 const startServer = async () => {
     await connectDB()
+    await walletCreatedEvent()
     app.listen(5003, () => {
-        console.log('server running on port 50003');
+        console.log('server running on port 5003');
     })
 }
 
