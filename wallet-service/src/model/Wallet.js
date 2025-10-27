03d29a92920e716557cb39989c50d2f6a8bef016
shamil <muhammedshamil470@gmail.com>
2025-10-27 20:39:18 +0530

    fix:change transaction type form wallet

--- wallet-service/src/model/Wallet.js
+++ wallet-service/src/model/Wallet.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose'
 const walletSchema = new mongoose.Schema({
     userId: { type: String, required: true },
     balance: { type: Number, default: 0 },
-    transactions: [{ type: Schema.Types.ObjectId, ref: "Transaction" }],
+    transactions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Transaction" }],
 },
     {
         timestamps: true,
