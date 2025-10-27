0ac9de43a08ebb0c47b8fa31db71f8ec88296244
shamil <muhammedshamil470@gmail.com>
2025-10-27 20:04:51 +0530

    Create: create wallet model

---
+++ wallet-service/src/model/Wallet.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+
+
+const walletSchema = new mongoose.Schema({
+    userId: { type: String, required: true },
+    balance: { type: Number, default: 0 },
+    transactions: [{ type: Schema.Types.ObjectId, ref: "Transaction" }],
+},
+    {
+        timestamps: true,
+        toJSON: {
+            virtuals: true,
+            versionKey: false,
+            transform: (doc, ret) => {
+                ret.id = ret._id,
+                    delete ret._id
+            }
+        },
+        toObject: {
+            virtuals: true,
+            versionKey: false,
+            transform: (doc, ret) => {
+                ret.id = ret._id,
+                    delete ret._id
+            }
+        }
+    }
+);
+
+
+const Wallet = mongoose.model('Wallet', walletSchema)
+
+export default Wallet
