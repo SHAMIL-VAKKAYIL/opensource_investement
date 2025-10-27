8996e32111553e255f8bb4e42a4fca00e7432954
shamil <muhammedshamil470@gmail.com>
2025-10-27 20:37:30 +0530

    create:create walletcreatio message

---
+++ wallet-service/src/kafka/consumer.js
@@ -0,0 +1,17 @@
+import { Kafka } from 'kafkajs'
+import WalletService from '../services/wallet.service.js'
+
+const kafka = new Kafka({ brokers: ['localhost:9092'] })
+const consumer = kafka.consumer({ groupId: 'wallet-service' })
+
+export const walletCreatedEvent = async () => {
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'wallet_creation' })
+
+    await consumer.run({
+        eachMessage: async ({ message }) => {
+            const data = JSON.parse(message.value.toString())
+            await WalletService.createWallet({ userId: data })
+        }
+    })
+}
