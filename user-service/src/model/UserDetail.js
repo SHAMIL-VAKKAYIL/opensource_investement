36ac8dc51cc6400a24a02eef2990df0381d5bd89
shamil <muhammedshamil470@gmail.com>
2025-10-27 19:53:29 +0530

    fix:userId type change to String

--- user-service/src/model/UserDetail.js
+++ user-service/src/model/UserDetail.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const userDataSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
+    userId: { type: String, required: true },
     name: { type: String },
     phone: { type: Number },
     address: { type: String },
