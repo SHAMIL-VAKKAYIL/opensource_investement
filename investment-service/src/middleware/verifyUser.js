0bea5272f4997cee398bee2b48bb7e167e4fffd0
shamil <muhammedshamil470@gmail.com>
2025-10-29 07:01:50 +0530

    create middleare for user verify

---
+++ investment-service/src/middleware/verifyUser.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken'
+import { errorResponse } from '../utils/response.util';
+
+
+export const verifyUser = async (req, res, next) => {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+        return errorResponse(res, 401, 'No token')
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.userId = decoded.userId;
+        next();
+
+    } catch (error) {
+        return errorResponse(res, 401, 'Invalid token')
+
+    }
+}
