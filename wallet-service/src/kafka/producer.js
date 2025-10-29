b58380da5800082bb5f319a0b468132970cf7ef5
shamil <muhammedshamil470@gmail.com>
2025-10-29 07:11:48 +0530

    create wallet created producer message to investmen service

---
+++ wallet-service/src/kafka/producer.js
@@ -0,0 +1,13 @@
+import { Kafka } from 'kafkajs'
+
+const kafka = new Kafka({ brokers: ['localhost:9092'] })
+const producer = kafka.producer()
+
+export const emitWalletCreated = async (data) => {
+    await producer.connect()
+    await producer.send({
+        topic: 'emit_wallet',
+        messages: [{ value: JSON.stringify(data) }]
+    })
+    await producer.disconnect()
+}
