71a1f51a43a6ff80f54fa52af08c5cfeea34cbea
shamil <muhammedshamil470@gmail.com>
2025-10-30 06:35:34 +0530

    fix : fix issue of wallet update when create new transaction

--- wallet-service/src/services/wallet.service.js
+++ wallet-service/src/services/wallet.service.js
@@ -1,14 +1,27 @@
 import { emitWalletCreated } from "../kafka/producer.js";
+import Transaction from "../model/Transaction.js";
 import Wallet from "../model/Wallet.js";
 
 class WalletService {
 
     async createWallet(data) {
-        // console.log(data, 'skjgfskdjfhncas');
-
         const newWallet = await Wallet.create({ userId: data.userId })
         await emitWalletCreated(newWallet)
         return newWallet
+    }
+    async newTransaction(data) {
+
+        if (!data.userId || !data.amount) {
+            throw new Error("invalid user id and amount");
+        }
+        const transaction = await Transaction.create({
+            userId: data.userId,
+            amount: data.amount,
+            type: 'debit'
+        })
+
+        const walletUpdate = await Wallet.updateOne({ userId: transaction.userId }, { $inc: { balance: -transaction?.amount },$push:{transactions:transaction.id} }, { new: true })
+        return walletUpdate
     }
 }
 
