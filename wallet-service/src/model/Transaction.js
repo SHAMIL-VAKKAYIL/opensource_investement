d4b45c3a2a58e75bc553a6631baad4b9b074f4f3
shamil <muhammedshamil470@gmail.com>
2025-10-30 06:36:12 +0530

    update:update on Transaction model and server js

--- wallet-service/src/model/Transaction.js
+++ wallet-service/src/model/Transaction.js
@@ -1,11 +1,9 @@
 import mongoose from "mongoose";
 
 const transactionSchema = new mongoose.Schema({
-    walletId: { type: mongoose.Schema.Types.ObjectId, ref: "Wallet" },
     userId: { type: String, required: true },
     amount: Number,
     type: { type: String, enum: ["credit", "debit"], required: true },
-    description: String,
     createdAt: { type: Date, default: Date.now },
 }, {
     toJSON: {
--- wallet-service/src/server.js
+++ wallet-service/src/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db.js'
-import { walletCreatedEvent } from './kafka/consumer.js'
+import { InvestmentCreatedEvent, walletCreatedEvent } from './kafka/consumer.js'
 import walletRouter from './routes/wallet.route.js'
 
 
@@ -12,12 +12,13 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use('/api/wallet',walletRouter)
+app.use('/api/wallet', walletRouter)
 
 
 const startServer = async () => {
     await connectDB()
     await walletCreatedEvent()
+    await InvestmentCreatedEvent()
     app.listen(5003, () => {
         console.log('server running on port 5003');
     })
