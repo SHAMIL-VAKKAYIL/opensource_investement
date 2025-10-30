7f451edf46fa3ead855395ea13e150969d1efc34
shamil <muhammedshamil470@gmail.com>
2025-10-30 06:34:52 +0530

    update:create Ivesetment created consumer

--- wallet-service/src/kafka/consumer.js
+++ wallet-service/src/kafka/consumer.js
@@ -2,9 +2,9 @@ import { Kafka } from 'kafkajs'
 import WalletService from '../services/wallet.service.js'
 
 const kafka = new Kafka({ brokers: ['localhost:9092'] })
-const consumer = kafka.consumer({ groupId: 'wallet-service' })
 
 export const walletCreatedEvent = async () => {
+    const consumer = kafka.consumer({ groupId: 'wallet-create-service' })
     await consumer.connect()
     await consumer.subscribe({ topic: 'wallet_creation' })
 
@@ -15,3 +15,21 @@ export const walletCreatedEvent = async () => {
         }
     })
 }
+
+
+
+export const InvestmentCreatedEvent = async () => {
+    const consumer = kafka.consumer({ groupId: 'Investment-create-event' })
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'create_investment' })
+
+    await consumer.run({
+        eachMessage: async ({ message }) => {
+            const data = JSON.parse(message.value.toString())            
+            await WalletService.newTransaction(data)
+        }
+    })
+
+}
+
+
