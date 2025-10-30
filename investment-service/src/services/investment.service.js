95dc30fa44945612479d7a23e5402a618ef61256
shamil <muhammedshamil470@gmail.com>
2025-10-30 06:28:44 +0530

    complete:create wallet and create investment services

---
+++ investment-service/src/services/investment.service.js
@@ -0,0 +1,38 @@
+import Investment from "../model/Investment.js";
+import WalletMapping from "../model/WalletMapping.js";
+
+class InvestmentServices {
+
+    async createWalletMap(data) {
+        console.log(data, 'dsffaf');
+
+        const { userId, id } = data
+        console.log(userId, id, 'hgdgdgc');
+
+        await WalletMapping.create({
+            userId,
+            walletId: id
+        })
+
+    }
+    async createInvestment({ userId, amount, planType, expectedReturn, durationDays }) {
+
+        const walletId = await WalletMapping.findOne({ userId })
+        const currentDate = new Date();
+        const endDate = new Date(currentDate.getTime() + durationDays * 24 * 60 * 60 * 1000);
+
+        const newInvestment = await Investment.create({
+            userId,
+            walletId: walletId?.walletId,
+            amount,
+            planType,
+            expectedReturn,
+            durationDays,
+            endDate
+        })
+
+        return newInvestment
+    }
+}
+
+export default new InvestmentServices
