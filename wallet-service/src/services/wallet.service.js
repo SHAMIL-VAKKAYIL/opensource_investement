5100f57474f5806d09f59842f205237dbfb9916c
shamil <muhammedshamil470@gmail.com>
2025-11-09 12:48:02 +0530

    create service for withdraw and get withdraw data

--- wallet-service/src/services/wallet.service.js
+++ wallet-service/src/services/wallet.service.js
@@ -1,6 +1,7 @@
 import { emitWalletCreated } from "../events/producer.js";
 import Transaction from "../model/Transaction.js";
 import Wallet from "../model/Wallet.js";
+import Withdraw from "../model/Withdrawal.js";
 
 class WalletService {
 
@@ -20,8 +21,63 @@ class WalletService {
             type: 'debit'
         })
 
-        const walletUpdate = await Wallet.updateOne({ userId: transaction.userId }, { $inc: { balance: -transaction?.amount },$push:{transactions:transaction.id} }, { new: true })
+        const walletUpdate = await Wallet.updateOne({ userId: transaction.userId }, { $inc: { balance: -transaction?.amount }, $push: { transactions: transaction.id } }, { new: true })
         return walletUpdate
+    }
+    async updateWalletAndTransaction(data) {
+        for (const returnData of data) {
+            const transaction = await Transaction.create({
+                userId: returnData.userId,
+                amount: returnData.returnAmount,
+                type: 'credit'
+            })
+            console.log(transaction);
+
+
+            await Wallet.updateOne({ userId: transaction.userId }, { $inc: { balance: transaction.amount }, $push: { transactions: transaction.id } })
+        }
+    }
+    async createWithdrawal(data) {
+        const { userId, amount } = data
+
+        const walletData = await Wallet.findOne({ userId })
+        if (!walletData) throw new Error('Wallet not found');
+
+        const enoughBalance = walletData.balance >= amount;
+
+        const status = enoughBalance ? 'success' : 'failed';
+
+
+        const withdrawData = await Withdraw.create({
+            userId,
+            amount,
+            status,
+            bankDetails,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        })
+
+        if (!enoughBalance) return withdrawData;
+
+        const transaction = await Transaction.create({
+            userId: withdrawData.userId,
+            amount: withdrawData.amount,
+            type: 'debit'
+        })
+
+        await Wallet.updateOne({ userId: transaction.userId }, { $inc: { balance: -transaction.amount }, $push: { transactions: transaction.id } })
+
+        return withdrawData
+
+    }
+    async getAllWithdrawals(userId) {
+        const withdrawls = await Withdraw.find({ userId })
+        if (!withdrawls) throw new Error('withdrawls not found');
+
+        return withdrawls
+
+
+
     }
 }
 
