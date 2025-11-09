804e8948c9d927876ea0f1c4ef810d6779a5458c
shamil <muhammedshamil470@gmail.com>
2025-11-09 12:47:37 +0530

    create routes for withdraw and get withdraw data

--- wallet-service/src/routes/wallet.route.js
+++ wallet-service/src/routes/wallet.route.js
@@ -1,9 +1,39 @@
 import express from 'express'
+import WalletService from '../services/wallet.service'
+import { errorResponse, successResponse } from '../utils/response.util'
+import { verifyUser } from '../middleware/verifyUser'
 
-const router =express.Router()
+const router = express.Router()
 
 
+router.post('/v1/withdraw', verifyUser, async (req, res) => {
+    const userId = req.userId
+    const { amount } = req.body
+    try {
 
+        const withdrawData = await WalletService.createWithdrawal({ amount, userId })
+
+        if (withdrawData.status === 'failed') {
+            return errorResponse(res, 400, 'withdrawal faild')
+        }
+        return successResponse(res, 201, `successfully withdraw the ${amount}`)
+    } catch (error) {
+        console.log(error)
+        return errorResponse(res, 500, 'server err')
+
+    }
+})
+router.get('/v1/withdraw', verifyUser, async (req, res) => {
+    const userId = req.userId
+    try {
+        const withdrawls = await WalletService.getAllWithdrawals(userId)
+        return successResponse(res, 200, withdrawls)
+    } catch (error) {
+        console.log(error)
+        return errorResponse(res, 500, 'server err')
+
+    }
+})
 
 
 export default router
