7ac0aa87b0fecaebfa3e35161f2860b511f800bf
shamil <muhammedshamil470@gmail.com>
2025-12-15 06:47:58 +0530

    create proxy middleware

--- user-service/src/server.js
+++ user-service/src/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import { connectDB } from './config/db.js'
 import userRouter from './routes/user.route.js'
 import { userCreateEvent } from './events/consumer.js'
+import { createProxyMiddleware } from 'http-proxy-middleware'
 
 
 dotenv.config()
@@ -12,6 +13,14 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+
+
+app.use('/api/user',
+    createProxyMiddleware({
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+    })
+)
 app.use('/api/user', userRouter)
 
 
