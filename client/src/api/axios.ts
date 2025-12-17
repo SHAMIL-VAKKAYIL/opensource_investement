5184ac5dbf947dede99d1e7baeb74ccdeb1f7447
shamil <muhammedshamil470@gmail.com>
2025-12-17 23:04:37 +0530

    add bearer token

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -10,6 +10,14 @@ const axiosInstance = axios.create({
     }
 })
 
-
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  console.log(token);
+  
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
 
 export default axiosInstance
