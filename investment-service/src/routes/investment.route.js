cd40678787ea503513328cb390cda13d8947fceb
shamil <muhammedshamil470@gmail.com>
2025-12-28 22:54:35 +0530

    create update investments and getinvestmetns

--- investment-service/src/routes/investment.route.js
+++ investment-service/src/routes/investment.route.js
@@ -15,6 +15,7 @@ router.post('/v1/newInvestment', verifyUser, async (req, res) => {
             errorResponse(res, 401, 'give valid infromations')
         }
         const investment = await InvestmentService.createInvestment({ userId, amount, planType, expectedReturn, durationDays })
+
         await InvestmentCreatedMessage({ userId, amount: investment.amount })
 
         return successResponse(res, 201, 'created investment')
@@ -29,6 +30,32 @@ router.get('/v1/active', async (req, res) => {
     try {
         const activeInvestments = await InvestmentService.activeInvestments()
         return successResponse(res, 200, activeInvestments)
+    } catch (error) {
+        console.log(error);
+        return errorResponse(res, 500, 'Internal server error')
+    }
+})
+
+router.get('/v1/investment', async (req, res) => {
+    try {
+
+        const Investments = await InvestmentService.Investments()
+        return successResponse(res, 200, Investments)
+    } catch (error) {
+        console.log(error);
+        return errorResponse(res, 500, 'Internal server error')
+    }
+})
+
+router.put('/v1/investment/:id', async (req, res) => {
+    const { id } = req.params    
+    const {status} = req.body
+    
+    try {
+
+        const updateStatus = await InvestmentService.UpdateStatus({ id,status })
+        return successResponse(res, 200, updateStatus)
+
     } catch (error) {
         console.log(error);
         return errorResponse(res, 500, 'Internal server error')
