ae17c305d47bd7b4f57dfa6c325977d33d494903
shamil <muhammedshamil470@gmail.com>
2025-12-29 22:43:55 +0530

    change prodcuer code

--- wallet-service/src/events/producer.js
+++ wallet-service/src/events/producer.js
@@ -1,98 +1,70 @@
-import { Kafka } from 'kafkajs'
-
-const kafka = new Kafka({ brokers: ['localhost:9092'] })
-const producer = kafka.producer()
+import { producer } from './connectKafkaProducer.js'
 
 export const emitWalletCreated = async (data) => {
-    await producer.connect()
-    await producer.send({
-        topic: 'emit_wallet',
-        messages: [{ value: JSON.stringify(data) }]
-    })
-    await producer.disconnect()
+  await producer.send({
+    topic: 'emit_wallet',
+    messages: [{ value: JSON.stringify(data) }],
+  })
 }
 
 export const withdrawalEvent = async (data) => {
-    await producer.connect()
-    await producer.send({
-        topic: 'notifications',
-        messages: [
-            {
-                key: 'withdrawal.completed',
-                value: JSON.stringify(data)
-            }
-        ]
-    })
+  await producer.send({
+    topic: 'notifications',
+    messages: [
+      {
+        key: 'withdrawal.completed',
+        value: JSON.stringify(data),
+      },
+    ],
+  })
 }
-
-
-
 
 export const investmentSuccessMessage = async (data) => {
     console.log(data);
-
-    await producer.connect()
-    await producer.send({
-        topic: 'investment_status',
-        messages: [
-            {
-                key: 'wallet.deducted',
-                value: JSON.stringify({ status: 'success', data })
-            }
-        ]
-    })
+    
+  await producer.send({
+    topic: 'investment_status',
+    messages: [
+      {
+        key: 'wallet.deducted',
+        value: JSON.stringify({ status: 'success', data }),
+      },
+    ],
+  })
 }
+
 export const investmentFailedMessage = async (data) => {
-    console.log(data);
-
-    await producer.connect()
-    await producer.send({
-        topic: 'investment_status',
-        messages: [
-            {
-                key: 'wallet.failed',
-                value: JSON.stringify({ status: 'failed', data })
-            }
-        ]
-    })
+  await producer.send({
+    topic: 'investment_status',
+    messages: [
+      {
+        key: 'wallet.failed',
+        value: JSON.stringify({ status: 'failed', data }),
+      },
+    ],
+  })
 }
-
-
-// export const depositEvent = async (data) => {
-//     await producer.connect()
-//     await producer.send({
-//         topic: 'deposite_success',
-//         messages: [
-//             {
-//                 key: 'deposit.completed',
-//                 value: JSON.stringify(data)
-//             }
-//         ]
-//     })
-// }
 
 export const depositSuccessMessage = async (data) => {
-    await producer.connect()
-    await producer.send({
-        topic: 'deposite_status',
-        messages: [
-            {
-                key: 'deposit.success',
-                value: JSON.stringify({ status: 'success', data})
-            }
-        ]
-    })
+  await producer.send({
+    topic: 'deposite_status',
+    messages: [
+      {
+        key: 'deposit.success',
+        value: JSON.stringify({ status: 'success', data }),
+      },
+    ],
+  })
 }
 
 export const depositFailedMessage = async (data) => {
-    await producer.connect()
-    await producer.send({
-        topic: 'deposite_status',
-        messages: [
-            {
-                key: 'deposit.failed',
-                value: JSON.stringify({ status: 'failed', data})
-            }
-        ]
-    })
+  await producer.send({
+    topic: 'deposite_status',
+    messages: [
+      {
+        key: 'deposit.failed',
+        value: JSON.stringify({ status: 'failed', data }),
+      },
+    ],
+  })
 }
